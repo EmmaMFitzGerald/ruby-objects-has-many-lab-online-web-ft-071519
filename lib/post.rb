faadc665e3c90c7af0cd76372b5e1b55fc10b431
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-01 16:18:04 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -21,6 +21,18 @@ class Post
     @author = author 
     #tell the artist to add this song to its list of songs
     author.posts << self
+    
+     describe '#author_name' do
+    it 'knows the name of its author' do
+      sophie = Author.new('Sophie')
+      post.author = sophie
+      expect(post.author_name).to eq('Sophie')
+    end
+    
+  def author_name
+    self = Author.new(name)
+    post.author  = self
+  end
 
   end
   
